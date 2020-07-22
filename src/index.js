91f73bd6571d3b1b2899e5035258136f4c830db9
yuanchang <yuan.chang@iwhalecloud.com>
2020-07-22 15:43:14 +0800

    3个小球的模型

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
 import {
   renderer,
   scene,
   camera,
   clock,
 } from './commonInit';
-import robotModel from './assets/RobotExpressive.glb';
 
-const btn = document.getElementById('btn');
+import ballModal from './assets/ball.glb';
 
 let mixer;
-let animations; // 模型上带的动画
+let EmptyAction;
+const btn = document.getElementById('btn');
 
 // 模型加载函数
 const loadModels = () => {
   const loader = new GLTFLoader();
-  loader.load(robotModel, (gltf) => {
+  loader.load(ballModal, (gltf) => {
     scene.add(gltf.scene);
-    animations = gltf.animations;
-    testFn(gltf.scene)
+    console.log(gltf)
+    EmptyAction = gltf.animations.find(item => item.name === 'EmptyAction');
+    mixer = new THREE.AnimationMixer(gltf.scene);
+    mixer.clipAction(EmptyAction).play();
   })
 }
 
@@ -33,45 +34,9 @@ const animate = () => {
   renderer.render(scene, camera);
 }
 
-let walkAn;
-
-const testFn = (model) => {
-  mixer = new THREE.AnimationMixer(model);
-  walkAn = animations.find(item => item.name === 'Walking');
-  mixer.clipAction(walkAn).play()
-}
-
 btn.onclick = () => {
-  mixer.clipAction(walkAn).fadeOut();
-  const danceAn = animations.find(item => item.name === 'Wave');
-  mixer.clipAction(danceAn).clampWhenFinished = true;
-  mixer.clipAction(danceAn).loop = THREE.LoopOnce;
-  mixer.clipAction(danceAn)
-    .reset()
-    .setEffectiveTimeScale(1)
-    .setEffectiveWeight( 1 )
-    .fadeIn(1)
-    .play();
-
-  const onFinished = () => {
-    mixer.removeEventListener('finished', onFinished);
-    mixer.clipAction(danceAn)
-      .fadeOut();
-    mixer.clipAction(walkAn)
-      .reset()
-      .setEffectiveTimeScale(1)
-      .setEffectiveWeight( 1 )
-      .fadeIn(1)
-      .play();
-  }
-
-  mixer.addEventListener('finished', onFinished)
+  mixer.clipAction(EmptyAction).fadeOut();
 }
-
-
-// ['Jump', 'Yes', 'No', 'Wave', 'Punch', 'ThumbsUp']
-// ['Idle', 'Walking', 'Running', 'Dance', 'Death', 'Sitting', 'Standing']
-
 
 loadModels();
 animate();
